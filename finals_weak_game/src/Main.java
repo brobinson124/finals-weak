54e89e7d2989b01e3ee3f3ae07795aa7cdcef753
Dylan Cole <dylan.cole5652@gmail.com>
2016-10-31 19:00:59 -0600

    changed source

--- finals_weak_game/src/Main.java
+++ finals_weak_game/src/Main.java
@@ -27,8 +27,8 @@ public class Main {
 			}
 			
 			if(glfwGetMouseButton(win, 0) == GL_TRUE){
-				color_red = 0;
-				color_blue = 1;
+				color_red = 0.5f;
+				color_blue = 0.5f;
 			}else {
 				color_red = 1;
 				color_blue = 0;
