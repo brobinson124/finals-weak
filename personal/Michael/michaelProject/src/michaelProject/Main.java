630932bd55296e411d432d2ecefa8cd7f43c3d02
Michael Feller <mfeller18@gmail.com>
2016-11-25 11:23:17 -0700

    Add progress up to tutorial video 10

--- personal/Michael/michaelProject/src/michaelProject/Main.java
+++ personal/Michael/michaelProject/src/michaelProject/Main.java
@@ -2,6 +2,9 @@ package michaelProject;
 
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
+
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
 import org.lwjgl.opengl.GL;
 
 public class Main {
@@ -20,11 +23,44 @@ public class Main {
 		
 		GL.createCapabilities();
 		
-		float color_red = 1;
-		float color_blue = 0;
+		Camera camera = new Camera(640, 480);
+		glEnable(GL_TEXTURE_2D);
+		
+		float[] vertices = new float[] {
+				-0.5f, 0.5f, 0, 	//top left		0
+				0.5f, 0.5f, 0, 		//top right		1
+				0.5f, -0.5f, 0, 	//bottom right	2
+				-0.5f, -0.5f, 0, 	//bottom left	3
+		};
+		
+		float[] texture = new float[] {
+				0,0,
+				1,0,
+				1,1,
+				0,1,
+		};
+		
+		int[] indices = new int[] {
+				0,1,2,
+				2,3,0
+		};
+		
+		Model model = new Model(vertices, texture, indices);
+		
+		Shader shader = new Shader("shader");
+		
+		Texture tex = new Texture("./resources/stoneTex.jpg");
+		
+		Matrix4f scale = new Matrix4f()
+				.translate(new Vector3f(100,0,0))
+				.scale(64);
+		
+		Matrix4f target = new Matrix4f();
+		
+		camera.setPosition(new Vector3f(-100,0,0));
 		
 		while(glfwWindowShouldClose(win) != true) {
-			
+			target = scale;
 			if(glfwGetKey(win, GLFW_KEY_ESCAPE) == GL_TRUE) {
 				glfwSetWindowShouldClose(win, true);
 			}
@@ -33,19 +69,11 @@ public class Main {
 			
 			glClear(GL_COLOR_BUFFER_BIT);
 			
-			glBegin(GL_QUADS);
-				glColor4f(color_red,0,color_blue,0);
-				glVertex2f(-0.5f, 0.5f);
-				
-				//glColor4f(0,1,0,0);
-				glVertex2f(0.5f, 0.5f);
-				
-				//glColor4f(0,0,1,0);
-				glVertex2f(0.5f, -0.5f);
-				
-				//glColor4f(1,1,1,0);
-				glVertex2f(-0.5f, -0.5f);
-			glEnd();
+			shader.bind();
+			shader.setUniform("sampler", 0);
+			shader.setUniform("projection", camera.getProjection().mul(target));
+			tex.bind(0);
+			model.render();
 			
 			glfwSwapBuffers(win);
 		}
